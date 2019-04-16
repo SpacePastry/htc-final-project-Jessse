c330ee85dc00b2c5721f98e7b1fc5922804016b2
User <User@DEV-022774.OnBase.net>
2019-04-16 18:00:24 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tc_final_project_jesse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
