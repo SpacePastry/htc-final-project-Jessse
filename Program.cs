b7627ae683a33d02e1a64a8a5e56ff433e1bda42
User <User@DEV-022774.OnBase.net>
2019-04-16 19:27:34 -0400

    second commit!

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Tc_final_project_jesse
 {
@@ -6,7 +7,27 @@ namespace Tc_final_project_jesse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int RsbCom = random.Next(1, 3);
+
+            int Rsb = 0;
+            Console.WriteLine("Hey, dude choose rock, paper or scissors, (all lower case pls)");
+            
+
+            numList<string> nmLst = new List<string> { "rock", "paper", "scissors" };
+
+
+            if (Rsb == "paper" && RsbCom == 3 ) {
+                Console.WriteLine("You win against the computer");
+            }
+
+           
+           
+           
+            // rr9 rp6 rs3 ss1 sr3 sp2 pp4 ps2 pr6 
+            //r 3
+            //p 2
+            //s 1
         }
     }
 }
