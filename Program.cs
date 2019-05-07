5c96252f691104581b706de8c49a9d358884de4d
User <User@TECH-018430.OnBase.net>
2019-05-07 17:21:47 -0400

    the (near finished) code

--- Program.cs
+++ Program.cs
@@ -9,25 +9,68 @@ namespace Tc_final_project_jesse
         {
             Random random = new Random();
             int RsbCom = random.Next(1, 3);
-
-            int Rsb = 0;
-            Console.WriteLine("Hey, dude choose rock, paper or scissors, (all lower case pls)");
+            Console.WriteLine("Would you like to start?");
+            while (Console.ReadLine() == "yes"){
             
+            Console.WriteLine("Hey, dude choose rock, paper or scissors, (all lower case pls)");
+            string Rsb = Console.ReadLine();
+            Console.WriteLine(RsbCom);
 
-            numList<string> nmLst = new List<string> { "rock", "paper", "scissors" };
+            List<string> nmLst = new List<string> { "rock", "paper", "scissors" };
 
 
-            if (Rsb == "paper" && RsbCom == 3 ) {
+
+            if (Rsb == "paper" && RsbCom == 3) {
                 Console.WriteLine("You win against the computer");
             }
+            else if (Rsb == "paper" && RsbCom == 2) {
+                Console.WriteLine("You tied with the computer");
+            }
+            else if (Rsb == "paper" && RsbCom == 1) {
+                Console.WriteLine("You lost against the computer");
+            }
+                        
+            
+            else if (Rsb == "rock" && RsbCom == 1 ) {
+                Console.WriteLine("You win against the computer");
+            }
+            else if (Rsb == "rock" && RsbCom == 3) {
+                Console.WriteLine("You tied with the computer");
+            }
+            else if (Rsb == "rock" && RsbCom == 2) {
+                Console.WriteLine("You lost against the computer");
+            }
 
-           
-           
-           
+
+            else if (Rsb == "scissors" && RsbCom == 2) {
+                Console.WriteLine("You win against the computer");
+            }
+            else if (Rsb == "scissors" && RsbCom == 1) {
+                Console.WriteLine("You tied with the computer");
+            }
+            else if (Rsb == "scissors" && RsbCom == 3) {
+                Console.WriteLine("You lost against the computer");
+            }
+            else {
+                Console.WriteLine("Please try again");
+            }
             // rr9 rp6 rs3 ss1 sr3 sp2 pp4 ps2 pr6 
             //r 3
             //p 2
             //s 1
+
+            Console.WriteLine("Would you like to play again?");
+            string yn = Console.ReadLine.
+
+            if (yn = "yes") {
+
+            }
+            else {
+                Console.WriteLine("Oh, ok then");
+                break;
+            }
+            
+            }
         }
     }
 }
